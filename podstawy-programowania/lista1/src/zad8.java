8147f5591839149a42efcc30df8359430848fc7b
kociola <mateusz.kociola@icloud.com>
2021-12-03 19:29:43 +0100

    zad 8

--- podstawy-programowania/lista1/src/zad8.java
+++ podstawy-programowania/lista1/src/zad8.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class zad8 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        int n1 = 0, n2 = 1, n3;
+
+        System.out.println("Podaj ile liczb z ciagu fibonacciego wyswietlic");
+        int how_many = sc.nextInt();
+
+        for(int i = 0; i < how_many; ++i) {
+
+            n3 = n1 + n2;
+            System.out.print(n3 + " ");
+            n1 = n2;
+            n2 = n3;
+
+        }
 
     }
 
